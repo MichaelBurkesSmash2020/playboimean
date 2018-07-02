a30a45e789f10aa84e33a8aa4581385e590ddc5e
MichaelBurkesSmash2020 <40174025+MichaelBurkesSmash2020@users.noreply.github.com>
2018-07-02 10:36:00 -0400

    commit message

---
+++ MAAAD(pollution)/js/operators.js
@@ -0,0 +1,11 @@
+function ppG(){
+
+    var miles = document.getElementById("miles").value;
+    var mpg= document.getElementById("mpg").value
+    var co2 = 20;
+    var gallons = miles /mpg;
+
+    var total = co2 * gallons;
+
+    document.getElementById("answer").value = total;
+}
--- README.md
+++
@@ -1,1 +0,0 @@
-"playbou mean" 
